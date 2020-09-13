fbcdd4f896e6dd3a7683d2972109cd33c4127bd0
GitHub Action <action@github.com>
2020-09-13 21:50:30 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/maxence-lefebvre/multifilestemplates/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.maxence-lefebvre.multifilestemplates.services
+
+import com.github.maxence-lefebvre.multifilestemplates.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/maxence-lefebvre/multifilestemplates/services/MyProjectService.kt
+++ src/main/kotlin/com/github/maxence-lefebvre/multifilestemplates/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.maxence-lefebvre.multifilestemplates.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.maxence-lefebvre.multifilestemplates.MyBundle
 
 class MyProjectService(project: Project) {
 
